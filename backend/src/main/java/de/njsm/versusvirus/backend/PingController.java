ceff83533d3b70217e1d989990bd23044491e393
Samuel Siebenmann <lv100lugitar@gmail.com>
2020-04-03 14:31:04 +0200

    Integrate frontend with backend
    * Update frontend ping api to call backend
    * Update backend ping controller to allow all cross origin requests
    * Update spring datasource configuration
    * Disable liquibase

--- backend/src/main/java/de/njsm/versusvirus/backend/PingController.java
+++ backend/src/main/java/de/njsm/versusvirus/backend/PingController.java
@@ -1,5 +1,7 @@
 package de.njsm.versusvirus.backend;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,7 +9,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/v1")
 public class PingController {
 
-    @RequestMapping("/ping")
+    @CrossOrigin
+    @GetMapping("/ping")
     public String ping() {
         return "Hello World";
     }
--- frontend/src/apis/ping.js
+++ frontend/src/apis/ping.js
@@ -1,13 +1,8 @@
-// import axios from 'axios';
+import axios from 'axios';
 
 import { BACKEND_URL } from '../config/constants';
 
 export async function ping() {
-    //const response = await axios.get(`${BACKEND_URL}/api/v1/ping`);
-    //return response.data;
-    console.log(BACKEND_URL);
-
-    const sleep = m => new Promise(r => setTimeout(r, m));
-    await sleep(1000);
-    return 'Hello World!';
+    const response = await axios.get(`${BACKEND_URL}/api/v1/ping`);
+    return response.data;
 }
