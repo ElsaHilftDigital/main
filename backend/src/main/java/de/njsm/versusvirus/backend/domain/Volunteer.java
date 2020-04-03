e6a52f29670de4231816ff512872b4c402d39af7
Nicolas Truessel <nicolas+git@ntruessel.ch>
2020-04-03 18:19:14 +0200

    Create Volunteer

---
+++ backend/src/main/java/de/njsm/versusvirus/backend/domain/Volunteer.java
@@ -0,0 +1,27 @@
+package de.njsm.versusvirus.backend.domain;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+
+@Entity
+public class Volunteer {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    private String name;
+    private String surname;
+    private String phone;
+    private String email;
+    private Address address;
+    private LocalDate birthDate;
+    private String iban;
+}
+
+@Embeddable
+class Address {
+    private String address;
+    private String City;
+    private String zipCode;
+}
