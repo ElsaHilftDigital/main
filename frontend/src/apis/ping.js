81b6817b9f8b0bc23d9b3fe04fffc64f12b8cd9f
Samuel Siebenmann <lv100lugitar@gmail.com>
2020-04-03 11:32:38 +0200

    Make ping api behave asynchronously
    * Make sure ping api does not respond immediately to showcast
      handling of asynchronous backend calls

--- frontend/src/apis/ping.js
+++ frontend/src/apis/ping.js
@@ -5,5 +5,8 @@ import { BACKEND_URL } from '../config/constants';
 export async function ping() {
     //const response = await axios.get(`${BACKEND_URL}/api/v1/ping`);
     //return response.data;
+
+    const sleep = m => new Promise(r => setTimeout(r, m));
+    await sleep(1000);
     return 'Hello World!';
 }
--- frontend/src/components/Ping.js
+++ frontend/src/components/Ping.js
@@ -6,16 +6,23 @@ import { pingActions, pingSelectors } from '../store/ping';
 const Ping = (props) => {
     const dispatch = useDispatch();
     const pingResponse = useSelector(pingSelectors.pingResponse);
+    const pingRequestOngoing = useSelector(pingSelectors.pingRequestOngoing);
     const pingServer = () => dispatch(pingActions.ping());
 
     return (
         <div className="container mt-5">
             <h1>Ping!</h1>
             <div className="d-flex flex-column justify-content-center">
-                {!pingResponse ? (
+                {pingRequestOngoing && (
+                    <div className="spinner-border text-primary mb-3" role="status">
+                        <span className="sr-only">Loading...</span>
+                    </div>  
+                )}
+                {!pingRequestOngoing && !pingResponse && (
                     <p>No response from the server yet. Try pinging it!</p>
-                ) : (
-                <p>The server responded with: {pingResponse}</p>
+                )}
+                {!pingRequestOngoing && pingResponse && (
+                    <p>The server responded with: {pingResponse}</p>
                 )}
                 <button 
                     onClick={pingServer} 
