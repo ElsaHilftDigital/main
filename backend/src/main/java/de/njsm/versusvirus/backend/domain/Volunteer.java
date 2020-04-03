0020f35a51088deb53c7915064f43856394f333b
Nicolas Truessel <nicolas+git@ntruessel.ch>
2020-04-03 19:26:58 +0200

    Enable Schema Validation and Fix Volunteer

--- backend/src/main/java/de/njsm/versusvirus/backend/domain/Volunteer.java
+++ backend/src/main/java/de/njsm/versusvirus/backend/domain/Volunteer.java
@@ -7,7 +7,7 @@ import java.time.LocalDate;
 public class Volunteer {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
 
     private String name;
---
+++ backend/src/main/resources/db/migrations/20200403-1917-fix-volunteer-table.sql
@@ -0,0 +1,2 @@
+alter table volunteer rename column zipCode to zip_code;
+alter table volunteer rename column birthDate to birth_date;
