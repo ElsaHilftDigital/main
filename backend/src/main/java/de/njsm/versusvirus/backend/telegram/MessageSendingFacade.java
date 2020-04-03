43d6e73b173bc969faa088b37768a87485347835
Jan Veen <jan.veen@posteo.de>
2020-04-03 23:12:34 +0200

    Telegram: Add two more needed messages

--- backend/src/main/java/de/njsm/versusvirus/backend/telegram/MessageSendingFacade.java
+++ backend/src/main/java/de/njsm/versusvirus/backend/telegram/MessageSendingFacade.java
@@ -2,6 +2,26 @@ package de.njsm.versusvirus.backend.telegram;
 
 public class MessageSendingFacade {
 
+    public void confirmPreliminaryRegistration(/*User*/) {
+        /*
+
+        if confirmed:
+        confirmRegistration()
+
+        else:
+
+        "you have been registered and we check your data"
+        -> tell them to wait
+
+         */
+    }
+
+    public void confirmRegistration(/*User*/) {
+        /*
+        You have been successfully registered. Join the group chat here (URL)
+         */
+    }
+
     public void broadcastPurchase(/*Purchase*/) {
 
     }
