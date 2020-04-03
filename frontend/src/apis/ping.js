7c8d571506bb30dad77004d028ee5391457d4ee8
Samuel Siebenmann <lv100lugitar@gmail.com>
2020-04-03 14:46:00 +0200

    Update development backend_url

--- frontend/src/apis/ping.js
+++ frontend/src/apis/ping.js
@@ -3,6 +3,6 @@ import axios from 'axios';
 import { BACKEND_URL } from '../config/constants';
 
 export async function ping() {
-    const response = await axios.get(`${BACKEND_URL}/api/v1/ping`);
+    const response = await axios.get(`${BACKEND_URL}/v1/ping`);
     return response.data;
 }
