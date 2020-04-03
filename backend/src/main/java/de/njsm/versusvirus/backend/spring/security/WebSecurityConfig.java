15c1dc99653fc2f8bbbee01550101932b4d8ced5
Nicolas Truessel <nicolas+git@ntruessel.ch>
2020-04-03 23:19:30 +0200

    Use BCrypt Passwords

--- backend/src/main/java/de/njsm/versusvirus/backend/spring/security/WebSecurityConfig.java
+++ backend/src/main/java/de/njsm/versusvirus/backend/spring/security/WebSecurityConfig.java
@@ -1,11 +1,14 @@
 package de.njsm.versusvirus.backend.spring.security;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
@@ -15,7 +18,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
         auth.inMemoryAuthentication()
                 .withUser("user")
-                .password("{noop}password")
+                .password(passwordEncoder().encode("password"))
                 .roles("USER");
     }
 
@@ -28,4 +31,10 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .antMatchers("/api/v1/anonymous/**").permitAll()
                 .anyRequest().authenticated();
     }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
 }
