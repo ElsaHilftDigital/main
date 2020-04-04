2d2db87c600caedacb3b54a75557a8cbc9d78575
Nicolas Truessel <nicolas+git@ntruessel.ch>
2020-04-04 22:51:16 +0200

    Improve Search Box

--- frontend/src/components/SearchBox.js
+++ frontend/src/components/SearchBox.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
+import useConstant from 'use-constant';
 
 const Container = styled.div`
     border: 1px solid #ccd4da !important;
@@ -13,14 +14,53 @@ const Container = styled.div`
     }
 `;
 
+function debounce(fn, delay) {
+	let timeoutId;
+	return function (t) {
+		clearTimeout(timeoutId);
+		timeoutId = setTimeout(() => fn(t), delay);
+	};
+}
 
-const SearchBox = () => {
+function compose(fn, mapper) {
+	return function (u) {
+		fn(mapper(u));
+	}
+}
+
+function deduplicate(fn) {
+	let cacheValue;
+	return function (t) {
+		if (cacheValue !== t) {
+			fn(t);
+		}
+		cacheValue = t;
+	}
+}
+
+const doNothing = () => {};
+
+const SearchBox = (props) => {
+    const onChange = useConstant(() => deduplicate(props.onChange ?? doNothing));
+    const debouncedOnChange = useConstant(() => debounce(onChange, 500));
+
+    const changeHandler = (e) => {
+		debouncedOnChange(e.target.value);
+	};
+
+	const keyDownHandler = (e) => {
+		if (e.key === "Enter") {
+			const value = e.currentTarget.value;
+			onChange(value);
+		}
+    };
+
     return (
         <Container className="input-group">
             <div className="input-group-prepend">
                 <i class="fa fa-search" style={{margin: 'auto 0', zIndex: 1000}}/>
             </div>
-            <input type="search" className="form-control" style={{border: 'none', outline: 'none', boxShadow: 'none'}}/>
+            <input onChange={onChange} onKeyDown={keyDownHandler} type="search" className="form-control" style={{border: 'none', outline: 'none', boxShadow: 'none'}}/>
         </Container>
     );
 };
