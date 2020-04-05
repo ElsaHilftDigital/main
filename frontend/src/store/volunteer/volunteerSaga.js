a65285daf2761d8dff562432212a569d42f6fc3c
Samuel Siebenmann <lv100lugitar@gmail.com>
2020-04-05 14:39:04 +0200

    Add missing histroy import to volunteerSaga

--- frontend/src/store/volunteer/volunteerSaga.js
+++ frontend/src/store/volunteer/volunteerSaga.js
@@ -1,5 +1,6 @@
 import { all, call, put, takeLatest } from 'redux-saga/effects';
 
+import history from '../../history';
 import * as actions from './volunteerActions';
 import { volunteerActions } from '.';
 
