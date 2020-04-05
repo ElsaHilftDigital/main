78c2879aaf38a600f23dd3f43d57a6691df418cd
Samuel Siebenmann <lv100lugitar@gmail.com>
2020-04-05 11:05:51 +0200

    Integrate createCustomer api

--- frontend/src/store/customer/customerActions.js
+++ frontend/src/store/customer/customerActions.js
@@ -2,6 +2,10 @@ export const GET_ALL_CUSTOMERS = 'GET_ALL_CUSTOMERS';
 export const GET_ALL_CUSTOMERS_SUCCESS = 'GET_ALL_CUSTOMERS_SUCCESS';
 export const SET_SELECTED_CUSTOMER = 'SET_SELECTED_CUSTOMER';
 
+export const CREATE_CUSTOMER = 'CREATE_CUSTOMER';
+export const CREATE_CUSTOMER_SUCCESS = 'CREATE_CUSTOMER_SUCCESS';
+export const CREATE_CUSTOMER_ERROR = 'CREATE_CUSTOMER_ERROR';
+
 export const getAllCustomers = () => ({
     type: GET_ALL_CUSTOMERS,
 });
@@ -13,4 +17,15 @@ export const getAllCustomersSuccess = customers => ({
 export const setSelectedCustomer = (uuid) => ({
     type: SET_SELECTED_CUSTOMER,
     payload: uuid,
-})
+});
+
+export const createCustomer = (customer) => ({
+    type: CREATE_CUSTOMER,
+    payload: customer,
+});
+export const createCustomerSuccess = () => ({
+    type: CREATE_CUSTOMER_SUCCESS,
+});
+export const createCustomerError = error => ({
+    type: CREATE_CUSTOMER_ERROR,
+});
--- frontend/src/store/customer/customerReducer.js
+++ frontend/src/store/customer/customerReducer.js
@@ -4,6 +4,9 @@ export const initialState = {
     getAllCustomersRequestOngoing: false,
     customers: [],
     selectedCustomer: null,
+    createCustomerRequestOngoing: false,
+    createCustomerSuccess: null,
+    createCustomerError: null,
 };
 
 export default function customerReducer(state = initialState, action) {
@@ -27,6 +30,27 @@ export default function customerReducer(state = initialState, action) {
             return {
                 ...state,
                 selectedCustomer: selectedCustomers.length ? selectedCustomers[0] : null,
+            };
+        case actions.CREATE_CUSTOMER:
+            return {
+                ...state,
+                createCustomerRequestOngoing: true,
+                createCustomerSuccess: null,
+                createCustomerError: null,
+            };  
+        case actions.CREATE_CUSTOMER_SUCCESS:
+            return {
+                ...state,
+                createCustomerRequestOngoing: false,
+                createCustomerSuccess: true,
+                createCustomerError: null,
+            };
+        case actions.CREATE_CUSTOMER_ERROR:
+            return {
+                ...state,
+                createCustomerRequestOngoing: false,
+                createCustomerSuccess: false,
+                createCustomerError: payload,
             };
         default:
             return state;
