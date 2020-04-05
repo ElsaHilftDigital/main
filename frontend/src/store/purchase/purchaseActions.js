1d36a8753bbfc4841b6c647cbe92e3dc552c7f13
Samuel Siebenmann <lv100lugitar@gmail.com>
2020-04-05 12:26:01 +0200

    Fix purchase action name

--- frontend/src/store/purchase/purchaseActions.js
+++ frontend/src/store/purchase/purchaseActions.js
@@ -1,6 +1,6 @@
 export const CREATE_PURCHASE = 'CREATE_PURCHASE';
 export const CREATE_PURCHASE_SUCCESS = 'CREATE_PURCHASE_SUCCESS';
-export const CREATE_PURCHASE_ERROR = 'CREATE_PURCHASE_SUCCESS';
+export const CREATE_PURCHASE_ERROR = 'CREATE_PURCHASE_ERROR';
 
 export const createPurchase = purchase => ({
     type: CREATE_PURCHASE,
