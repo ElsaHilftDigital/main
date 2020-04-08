4795b5f2969a764e8d10e2c1edcb6358255e2624
Jan Veen <jan.veen@posteo.de>
2020-04-08 23:24:24 +0200

    Issue #23 Fix texts
    Remove username as it might be null
    Translate yes and no

--- backend/src/main/java/de/njsm/versusvirus/backend/telegram/MessageSender.java
+++ backend/src/main/java/de/njsm/versusvirus/backend/telegram/MessageSender.java
@@ -264,7 +264,6 @@ public class MessageSender {
         var forwardedMessage = MessageFormat.format(telegramMessages.getForwardedMessage(),
                 message.getFrom().getFirstName(),
                 message.getFrom().getLastName(),
-                message.getFrom().getUserName(),
                 message.getText());
 
         var m = new MessageToBeSent(chatId, forwardedMessage);
