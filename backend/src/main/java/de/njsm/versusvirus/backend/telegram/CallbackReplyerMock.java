f5fc4ddd01d604bc5d6278175f5e5be3e80abb00
Jan Veen <jan.veen@posteo.de>
2020-04-10 10:00:29 +0200

    Inject other mock in dev as well

--- backend/src/main/java/de/njsm/versusvirus/backend/telegram/CallbackReplyerMock.java
+++ backend/src/main/java/de/njsm/versusvirus/backend/telegram/CallbackReplyerMock.java
@@ -3,7 +3,11 @@ package de.njsm.versusvirus.backend.telegram;
 import de.njsm.versusvirus.backend.telegram.dto.CallbackQueryAnswer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Component;
 
+@Component
+@Profile("dev")
 public class CallbackReplyerMock implements CallbackQueryReplyer {
 
     private static final Logger LOG = LoggerFactory.getLogger(CallbackReplyerMock.class);
