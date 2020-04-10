a982020c929a8289bb849a67160fb342a4a5d877
Nicolas Truessel <nicolas+git@ntruessel.ch>
2020-04-10 02:15:56 +0200

    Revert "Add JWT Auth"
    This reverts commit 2c5e487da393778b62ffb7e5630c79b00cc3d381.

--- frontend/admin/src/components/App.js
+++ frontend/admin/src/components/App.js
@@ -3,6 +3,10 @@ import { Provider } from 'react-redux';
 import { Router, Route, Switch } from 'react-router-dom';
 
 import history from '../history';
+import About from './public/About';
+import Home from './public/Home';
+import Login from './public/Login';
+import RegisterVolunteer from './public/registration/RegisterVolunteer';
 import AdminLogin from './admin/home/AdminLogin';
 import Customer from './admin/customer/Customer';
 import Volunteer from './admin/volunteer/Volunteer';
@@ -10,6 +14,8 @@ import NewRequest from './admin/new-request/NewRequest';
 import PurchaseList from './admin/purchase/PurchaseList';
 import PurchaseDetail from './admin/purchase/PurchaseDetail';
 import Header from './Header';
+import PrivateRoute from './PrivateRoute';
+import LoginRoute from './LoginRoute';
 
 
 const App = (props) => {
@@ -18,12 +24,16 @@ const App = (props) => {
             <Router history={history}>
                 <Route path="/" component={Header} />
                 <Switch>
-                    <Route exact path="/admin" component={AdminLogin} />
-                    <Route exact path="/admin/new-request" component={NewRequest} />
-                    <Route exact path="/admin/purchases" component={PurchaseList}/>
-                    <Route exact path="/admin/purchase/:purchaseId" component={PurchaseDetail}/>
-                    <Route exact path="/admin/customers" component={Customer} />
-                    <Route exact path="/admin/volunteers" component={Volunteer} />
+                    <Route exact path="/" component={Home} />
+                    <Route exact path="/about" component={About} />
+                    <Route exact path="/register" component={RegisterVolunteer} />
+                    <Route exact path="/login" component={Login} />
+                    <PrivateRoute exact path="/admin/new-request" component={NewRequest} />
+                    <PrivateRoute exact path="/admin/purchases" component={PurchaseList}/>
+                    <PrivateRoute exact path="/admin/purchase/:purchaseId" component={PurchaseDetail}/>
+                    <PrivateRoute exact path="/admin/customers" component={Customer} />
+                    <PrivateRoute exact path="/admin/volunteers" component={Volunteer} />
+                    <LoginRoute exact path="/admin" component={AdminLogin} />
                 </Switch>
             </Router>
         </Provider>
