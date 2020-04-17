d33e89386ed8971e5b55e9c226aa369b28b315c1
Rakshi <rakshi00@gmail.com>
2020-04-17 16:52:52 +0200

    Add more attributes in volunteerList

--- frontend/admin/src/components/admin/volunteer/VolunteerList.tsx
+++ frontend/admin/src/components/admin/volunteer/VolunteerList.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import { useHistory } from 'react-router-dom';
-import { Col, ListGroup, Row } from 'react-bootstrap';
+import { Col, ListGroup, Row, Form } from 'react-bootstrap';
 
 import * as routes from 'routes';
 import { useVolunteers } from 'hooks/useVolunteers';
 import StatusIndicator from 'components/StatusIndicator';
+
 
 const VolunteerList: React.FC = () => {
     const { volunteers } = useVolunteers();
@@ -25,6 +26,7 @@ const VolunteerList: React.FC = () => {
 
     return (
         <>
+            <h2 style={{padding: "1rem", height: "5rem", display: "table-cell"}}>Helfer</h2>
             <ListGroup>
                 {sortedVolunteers.map((volunteer: any) => <CustomerListItem volunteer={volunteer} key={volunteer.uuid} />)}
             </ListGroup>
@@ -39,18 +41,68 @@ const statusIndicatorColor = (volunteer: any) => {
     return "RED";
 };
 
+const statusIndicator = (volunteer: any) => {
+    if (volunteer.validated) {
+        return "bestätigt";
+    }
+    return "nicht bestätigt";
+}
+
 interface ListItemProps {
     volunteer: any
 }
 const CustomerListItem: React.FC<ListItemProps> = (props) => {
     const { volunteer } = props;
     const history = useHistory();
+    const labelWidth = 5;
 
     return <ListGroup.Item action
         onClick={() => history.push(routes.volunteerDetails(volunteer.uuid))}
     >
         <Row>
-            <Col>{volunteer.lastName}</Col>
+            
+            <Col>
+                <Row>
+                    <Form.Label column md={labelWidth}>Name</Form.Label>
+                    <Col>
+                        <Form.Control plaintext readOnly defaultValue={volunteer.firstName + " " + volunteer.lastName} />
+                    </Col>
+                </Row>
+                <Row>
+                    <Form.Label column md={labelWidth}>Geburtstag</Form.Label>
+                    <Col>
+                        <Form.Control plaintext readOnly defaultValue={volunteer.birthDate} />
+                    </Col>
+                </Row>
+            </Col>
+            <Col>
+                <Row>
+                    <Form.Label column md={labelWidth}>Telefonnummer</Form.Label>
+                    <Col>
+                        <Form.Control plaintext readOnly defaultValue={volunteer.phone} />
+                    </Col>
+                </Row>
+                <Row>
+                    <Form.Label column md={labelWidth}>Email</Form.Label>
+                    <Col>
+                        <Form.Control plaintext readOnly defaultValue={volunteer.email} />
+                    </Col>
+                </Row>
+            </Col>
+            <Col>
+                <Row>
+                    <Form.Label column md={labelWidth}>Status</Form.Label>
+                    <Col>
+                        <Form.Control plaintext readOnly defaultValue={statusIndicator(volunteer)} />
+                    </Col>
+                </Row>
+                <Row>
+                    <Form.Label column md={labelWidth}>Ort</Form.Label>
+                    <Col>
+                        <Form.Control plaintext readOnly defaultValue={volunteer.zipCode + " " + volunteer.city} />
+                    </Col>
+                </Row>
+            </Col>
             <Col><StatusIndicator value={statusIndicatorColor(volunteer)} /></Col>
         </Row>
     </ListGroup.Item>
