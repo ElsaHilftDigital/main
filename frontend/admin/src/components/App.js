0def3dbeb2055b5bf568f4f2e1832ecbc96c3dc5
Nicolas Truessel <nicolas+git@ntruessel.ch>
2020-04-19 20:51:34 +0200

    Detect Login State From Cookies

--- frontend/admin/src/components/App.js
+++ frontend/admin/src/components/App.js
@@ -12,6 +12,7 @@ import NewRequest from './admin/new-request/NewRequest';
 import PurchaseList from './admin/purchase/PurchaseList';
 import PurchaseDetail from './admin/purchase/PurchaseDetail';
 import Header from './Header';
+import PrivateRoute from './PrivateRoute';
 
 
 const App = (props) => {
@@ -21,13 +22,13 @@ const App = (props) => {
                 <Route path="/" component={Header}/>
                 <Switch>
                     <Route exact path="/login"><AdminLogin/></Route>
-                    <Route exact path="/new-request"><NewRequest/></Route>
-                    <Route exact path="/purchases"><PurchaseList/></Route>
-                    <Route exact path="/purchase/:purchaseId"><PurchaseDetail/></Route>
-                    <Route exact path="/customers"><CustomerList/></Route>
-                    <Route exact path="/customer/:customerId"><CustomerDetail/></Route>
-                    <Route exact path="/volunteers"><VolunteerList/></Route>
-                    <Route exact path="/volunteer/:volunteerId"><VolunteerDetail/></Route>
+                    <PrivateRoute exact path="/new-request"><NewRequest/></PrivateRoute>
+                    <PrivateRoute exact path="/purchases"><PurchaseList/></PrivateRoute>
+                    <PrivateRoute exact path="/purchase/:purchaseId"><PurchaseDetail/></PrivateRoute>
+                    <PrivateRoute exact path="/customers"><CustomerList/></PrivateRoute>
+                    <PrivateRoute exact path="/customer/:customerId"><CustomerDetail/></PrivateRoute>
+                    <PrivateRoute exact path="/volunteers"><VolunteerList/></PrivateRoute>
+                    <PrivateRoute exact path="/volunteer/:volunteerId"><VolunteerDetail/></PrivateRoute>
                     <Redirect to="/purchases"/>
                 </Switch>
             </Router>
---
+++ frontend/admin/src/components/PrivateRoute.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Route, Redirect } from 'react-router-dom';
+
+const PrivateRoute: React.FC = ({ children, ...rest }) => {
+    const isAuthenticated = document.cookie.indexOf('token') != -1;
+    return isAuthenticated
+        ? <Route {...rest}>{children}</Route>
+        : <Redirect to="/login"/>;
+};
+
+export default PrivateRoute;
