5093c78fe466f8689381ad12d7fbd025f3d866c2
Rakshi <rakshi00@gmail.com>
2020-05-09 11:24:05 +0200

    Fix update-supermarket migration

--- backend/src/main/resources/db/migrations/20200509-1051-update-supermarket-uuid-not-null.sql
+++ backend/src/main/resources/db/migrations/20200509-1051-update-supermarket-uuid-not-null.sql
@@ -1,5 +1,7 @@
+create extension if not exists "uuid-ossp";
+
 update purchase_supermarket
-set uuid = uuid_generate_v4 ()
-where uuid = null;
+set uuid = uuid_generate_v4()
+where uuid is null;
 
 alter table purchase_supermarket alter column uuid set not null;
